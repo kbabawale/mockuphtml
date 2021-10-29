231fd6cc9ff7ae41147159f403278981c1805989
kbabawale <kolapobabawale@gmail.com>
2021-10-29 18:00:42 +0100

    wrapup

--- src/app/jumbotron/jumbotron.component.ts
+++ src/app/jumbotron/jumbotron.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Quote {
+  name: string;
+  imageUrl: string;
+}
+
 @Component({
   selector: 'app-jumbotron',
   templateUrl: './jumbotron.component.html',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JumbotronComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
+
+  quotes: Quote[] = [];
+  currentQuote: Quote = this.quotes[0];
+  counter: number = 0;
 
   ngOnInit(): void {
+    this.quotes = [
+      {
+        name: 'A life spent making mistakes is not only more honorable, but more useful than a life spent doing nothing',
+        imageUrl: '../../assets/images/man.png'
+      },
+      {
+        name: 'Quote 2',
+        imageUrl: '../../assets/images/womanslide.jpeg'
+      },
+      {
+        name: 'Quote 3',
+        imageUrl: '../../assets/images/womanslide2.png'
+      }
+    ];
+    setInterval(() => {
+
+      if (this.counter < this.quotes.length) {
+        this.currentQuote = this.quotes[this.counter];
+
+        this.counter === this.quotes.length - 1 ? this.counter = 0 : this.counter++;
+      }
+    }, 3000);
   }
 
 }
